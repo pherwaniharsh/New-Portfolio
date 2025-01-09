efaf18ef56a9a122d26aa7e98976c04b4bb07f97
pherwaniharsh <rmpherwani.hp@protonmail.com>
2025-01-09 18:53:12 +0530

    Fixed responsive navigation

--- pp.js
+++ pp.js
@@ -3,4 +3,12 @@ let navBar = document.querySelector("nav");
 
 menuButton.addEventListener('click', function(){
     navBar.classList.toggle("menuDisplay");
+    
+    if(navBar.classList.contains("menuDisplay")){
+        menuButton.classList.add("fixedButton");
+    }
+    else{
+        menuButton.classList.remove("fixedButton");
+    }
 })
+
